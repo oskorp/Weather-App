471e1221262e428e49a1d65ab9511106cb77807f
omkar khandalkar <53345307+oskorp@users.noreply.github.com>
2021-10-15 13:26:50 +0530

    Error handing for weatherapi

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const request = require('request')
 
 
-const url ="http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query=Mumbai"
+const url = "http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query="
 
-request({url : url, json : true},(error,response)=>{
-    console.log("Tempreture in mumbai is "+ response.body.current.temperature)
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log("Unable to connect please check your network")
+    } else if (response.body.error) {
+        console.log("Unable to find location")
+    }
+    else {
+        console.log("Tempreture in mumbai is " + response.body.current.temperature)
+
+    }
+
 })
+
+
+// const newUrl = "http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query=Delhi"
+// request({ url: url, json: true }, (error, response) => {
+//     const long = response.body.location.lat
+//     const lati = response.body.location.long
+//     console.log("longitude of delhi is " + long + "Latitude of delhi is" + lati)
+// })
