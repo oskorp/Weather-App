3aefd3b0a3388d201c13f9aeda37dd4f81aeb0b6
omkar khandalkar <53345307+oskorp@users.noreply.github.com>
2021-10-19 19:49:40 +0530

    created a function module and importing in app.js
    this will show you geocode of that place

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const request = require('request')
 
 
-const url = "http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query="
+// const url = "http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query="
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect please check your network")
-    } else if (response.body.error) {
-        console.log("Unable to find location")
-    }
-    else {
-        console.log("Tempreture in mumbai is " + response.body.current.temperature)
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log("Unable to connect please check your network")
+//     } else if (response.body.error) {
+//         console.log("Unable to find location")
+//     }
+//     else {
+//         console.log("Tempreture in mumbai is " + response.body.current.temperature)
 
-    }
+//     }
 
-})
+// })
 
 
 // const newUrl = "http://api.weatherstack.com/current?access_key=1889687260abb311449f6b591f74a4b1&query=Delhi"
@@ -23,3 +23,9 @@ request({ url: url, json: true }, (error, response) => {
 //     const lati = response.body.location.long
 //     console.log("longitude of delhi is " + long + "Latitude of delhi is" + lati)
 // })
+
+
+geocode('Goa', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
